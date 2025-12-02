d57705f0344189b96d9808402bb3e15a84c767ab
yello <yello.3617@gmail.com>
2025-12-03 00:27:03 +0900

    vm_alloc_page_with_initializer

--- pintos/vm/vm.c
+++ pintos/vm/vm.c
@@ -44,21 +44,43 @@ static struct frame *vm_evict_frame (void);
 /* Create the pending page object with initializer. If you want to create a
  * page, do not create it directly and make it through this function or
  * `vm_alloc_page`. */
-bool
-vm_alloc_page_with_initializer (enum vm_type type, void *upage, bool writable,
-		vm_initializer *init, void *aux) {
-
+// 초기화 프로그램을 사용하여 보류 중인 페이지 개체를 만듭니다.
+// 페이지를 만들고 싶다면 직접 만들지 말고 이 함수나 `vm_alloc_page`를 통해 만들어주세요.
+bool vm_alloc_page_with_initializer (enum vm_type type, void *upage, bool writable,	vm_initializer *init, void *aux) {
 	ASSERT (VM_TYPE(type) != VM_UNINIT)
 
 	struct supplemental_page_table *spt = &thread_current ()->spt;
+	bool (*initializer) (struct page *, enum vm_type, void *kva);
 
 	/* Check wheter the upage is already occupied or not. */
+	// upage가 이미 사용 중인지 확인하세요.
 	if (spt_find_page (spt, upage) == NULL) {
 		/* TODO: Create the page, fetch the initialier according to the VM type,
 		 * TODO: and then create "uninit" page struct by calling uninit_new. You
 		 * TODO: should modify the field after calling the uninit_new. */
 
+		struct page *page = malloc(sizeof(struct page));
+		if (page == NULL)
+			goto err;
+
+		if (VM_TYPE(type) == VM_ANON) {
+			initializer = anon_initializer;
+		} else if (VM_TYPE(type) == VM_FILE) {
+			initializer = file_backed_initializer;
+		} else {
+			free(page);
+			goto err;
+		}
+
+		uninit_new(page, upage, init, type, aux, initializer);
+		page->writable = true;
+
 		/* TODO: Insert the page into the spt. */
+		bool sip = spt_insert_page(spt, page);
+		if (sip == false)
+			goto err;
+
+		return true;
 	}
 err:
 	return false;
@@ -134,6 +156,7 @@ static struct frame *vm_get_frame (void) {
 	}
 
 	frame = malloc(sizeof(struct frame));
+
 	frame->kva = f_page;
 
 	ASSERT (frame != NULL);
@@ -179,6 +202,7 @@ bool vm_claim_page (void *va) {
 	struct thread *cur = thread_current();
 
 	page = spt_find_page(cur->spt, va);
+	// 위임을 핫ㅆ다..?
 	if(page == NULL)
 		return false;
 
